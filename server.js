eb7c74d144a889be1503ca321253784adb8c789a
Andrew Matysiak <andrewmatysiak@hotmail.com>
2024-03-11 16:01:27 +1100

    Setup web-server connection to mongoDB

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 console.log('Hello');
 
 const express = require('express');
+const { MongoDBCollectionNamespace } = require('mongodb');
+const mongoose= require('mongoose');
 const app = express();
 
 let PORT = 3000;
@@ -14,3 +16,12 @@ app.listen(PORT, () => {
 app.get('/', (req, res) => {
   res.send('Hello from SERVER NODE API')
 });
+
+
+mongoose
+.connect('mongodb+srv://andrew:0102030405@cluster0.y3goqgw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+.then( () => {
+  console.log('Connected to mongoDB...');
+}).catch( (err) => {
+  console.log(`ERROR connecting to MongoDB...`, err);
+})
